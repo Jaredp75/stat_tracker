85338cc2617213116b2d4b3bd0f9add69ebe9546
Jaredp75 <jaredp75@hotmail.com>
2017-09-10 21:41:13 -0400

    Another commit

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.set('views', './views');
 
 
 
-
+// put routes here
 
 app.use('/', rootRouter);
 app.use('/api', apiRouter);
@@ -62,3 +62,136 @@ app.post('/add_task/', function(req, res) {
     res.redirect('/');
   });
 });
+
+app.get("/activities/:id", function  (req, res) {
+  req.session.activityid = req.params.id;
+  console.log("ID: " + req.params.id);
+  User.findOne({_id: req.params.id}).then(function(user) {
+    res.render('addEntry', {user: user});
+  });
+});
+
+app.post("/api/activities/:id", function  (req, res) {
+  let activity = req.body.activity;
+  let id = req.params.id;
+  console.log("ID: "+req.params.id+", Activity: "+activity);
+  User.findOneAndUpdate({_id: req.params.id}, {activity: activity}).then(function(user) {
+    console.log("updated");
+    res.redirect('/activities');
+  });
+});
+
+
+
+app.post("/api/activities/:id/delete", function  (req, res) {
+	let id = req.params.id;
+  let query = {_id:id};
+  console.log("Query: "+query);
+  User.remove(query).then(function(user) {
+    console.log("deleted");
+    res.redirect('/activities');
+  });
+
+
+  app.post("/api/activities/:id/stats", function  (req, res) {
+    let id = req.params.id;
+    let activity = req.body.activity;
+  	let amount = req.body.amount;
+  	let newstat = {"activityId": req.body.activity, "identifier": id, "amount": req.body.amount, "create_date": req.body.date}
+    console.log(req.body);
+    console.log("ID: "+req.params.id+", Activity: "+req.body.activity+", Amount: "+req.body.amount+", Date: "+req.body.date);
+    Stat.create(newstat).then(function(user) {
+      console.log("added a stat");
+      res.redirect('/activities');
+    });
+  });
+
+  app.post("/api/stats/:id", function  (req, res) {
+	let id = req.params.id;
+  let query = {_id:id};
+  Stat.remove(query).then(function(stat) {
+    console.log("deleted stat");
+    res.redirect('/activities');
+  });
+});
+
+
+
+
+app.listen(port, function () {
+    console.log('Successfully started express application!')
+});
+
+
+
+
+
+
+
+
+// put routes here
+
+//app.get('/favicon.ico', function(req, res) {
+//  res.status(204);
+//})
+
+//app.get('/add/', function (req, res) {
+//  res.render('add_player');
+//});
+
+//app.post('/add/', function (req, res) {
+//  Player.create(req.body)
+//  .then(function (player) {
+//    res.redirect('/');
+//  })
+//});
+
+
+//app.get('/:id/', function (req, res) {
+//  Player.findOne({_id: req.params.id}).then(function (player) {
+//    res.render("player_profile", {player: player});
+//  })
+//});
+
+//app.get('/:id/update/', function (req, res) {
+//  Player.findOne({_id: req.params.id}).then(function (player) {
+//    res.render("update_player_profile", {player: player});
+//  })
+//});
+
+
+
+
+//app.get('/:id/update/', function (req, res) {
+//  Player.findOneAndUpdate({_id: req.params.id}, req,body).then(function (player) {
+//    res.redirect("/");
+//  })
+//})
+
+//app.get('/:id/add_stats/', function (req, res) {
+//  Player.findOne({_id: req.params.id}).then(function (player) {
+//    res.render("add_stats", {player: player});
+//  })
+//})
+
+//app.post('/:id/add_stats/', function (req, res) {
+//  Player.findOne({_id: req.params.id}).then(function (player) {
+//    player.stats.push(req.body);
+//    player.save().then(function () {
+//      res.render("add_stats", {player: player});
+//    })
+//  })
+//})
+
+//app.post('/:id/delete', function (req, res) {
+//  Player.findOneAndRemove({_id: req.params.id}).then(function (player) {
+//    res.redirect('/');
+//  })
+//});
+
+
+//app.get('/', function (req, res) {
+//  Player.find().then(function (player) {
+//    res.render('index', {player: player});
+//  })
+//})
